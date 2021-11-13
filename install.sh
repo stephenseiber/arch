1b57e6343619d6ae1b6522fdd4ba17e5f3e49adb
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-11-13 14:27:43 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -178,7 +178,7 @@ echo -en "$user_password\n$user_password" | passwd $username
 
 useradd -g users -G wheel -m temp
 sudo -u temp mkdir -p /tmp/yay && cd /tmp/yay && sudo -u temp git clone https://aur.archlinux.org/yay.git && cd yay && sudo -u temp makepkg -si --noconfirm
-sudo -u temp mkdir -p /tmp/epson && cd /tmp/epson && sudo -u temp git clone https://aur.archlinux.org/epson-inkjet-printer-escpr.git && cd epson && sudo -u temp makepkg -si --noconfirm
+sudo -u temp mkdir -p /tmp/epson && cd /tmp/epson && git clone https://aur.archlinux.org/epson-inkjet-printer-escpr.git && cd epson-inkjet-printer-escpr && sudo -u temp makepkg -si --noconfirm
 sudo -u temp mkdir -p /tmp/ttf-ms-fonts && cd /tmp/ttf-ms-fonts && sudo -u temp git clone https://aur.archlinux.org/ttf-ms-fonts.git && cd ttf-ms-fonts && sudo -u temp makepkg -si --noconfirm
 cd /tmp && touch panel-restart && echo '#!/bin/bash' > panel-restart && echo 'killall plasmashell;plasmashell &' >> panel-restart && chmod +x panel-restart && mv panel-restart /usr/bin/
 touch reflector-update && echo '#!/bin/bash' > reflector-update && echo 'sudo reflector --latest 50 --verbose --protocol https --sort rate --save /etc/pacman.d/mirrorlist -c US --ipv6' >> reflector-update && chmod +x reflector-update && mv reflector-update /usr/bin
