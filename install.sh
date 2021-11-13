3d34bfbcaa84e3b25a21cc539b196515ac78ae4f
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-11-13 10:11:45 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,16 @@ set -u  # Treat unset variables as an error when substituting
 
 ## This are the defaults, so it's easier to test the script
 # keymap=us
+# part=yes
 # username=csjarchlinux  # Can only be lowercase and no signs
 # hostname=desktop
 # user_password=csjarchlinux
 # root_password=csjarchlinux
+
+read -p "do you want to wipe full drive yes or no, or press enter to use defaults: " part
+if [[ -z $part ]]; then
+    part=yes
+fi
 
 read -p "Enter keymap, or press enter to use defaults: " keymap
 if [[ -z $keymap ]]; then
@@ -40,47 +46,84 @@ fi
 
 timedatectl set-ntp true  # Synchronize motherboard clock
 
-pacman -Sy dialog --noconfirm                                                                  #install dialog for selecting disk
-devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)                 #gets disk info for selection
-drive=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1       #chose which drive to format
-clear           # clears blue screen from 
-lsblk           # shows avalable drives
-echo ${drive}   # confirms drive selection
-
-sgdisk --zap-all ${drive}  # Delete tables
-printf "n\n1\n\n+333M\nef00\nn\n2\n\n\n\nw\ny\n" | gdisk ${drive}  # Format the drive
-
-part_boot="$(ls ${drive}* | grep -E "^${drive}p?1$")"     #finds boot partion
-part_root="$(ls ${drive}* | grep -E "^${drive}p?2$")"     #finds root partion 
-
-echo ${part_boot} # confirms boot partion selection
-echo ${part_root} # confirms root partion selection
-
-mkdir -p -m0700 /run/cryptsetup  # Change permission to root only
-cryptsetup luksFormat --type luks2 ${part_root}
-cryptsetup luksOpen ${part_root} cryptroot  # Open the mapper
-
-mkfs.vfat -F32 ${part_boot}  # Format the EFI partition
-mkfs.btrfs /dev/mapper/cryptroot  # Format the encrypted partition
-
-mount /dev/mapper/cryptroot /mnt
-btrfs subvolume create /mnt/@
-btrfs subvolume create /mnt/@home
-btrfs subvolume create /mnt/@pkg
-btrfs subvolume create /mnt/@var
-btrfs subvolume create /mnt/@srv
-btrfs subvolume create /mnt/@tmp
-umount /mnt
-
-mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@ /dev/mapper/cryptroot /mnt
-mkdir -p /mnt/{home,var/cache/pacman/pkg,var,srv,tmp,boot}  # Create directories for each subvolume
-mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@home /dev/mapper/cryptroot /mnt/home
-mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@pkg /dev/mapper/cryptroot /mnt/var/cache/pacman/pkg
-mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@var /dev/mapper/cryptroot /mnt/var
-mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@srv /dev/mapper/cryptroot /mnt/srv
-mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@tmp /dev/mapper/cryptroot /mnt/tmp
-chattr +C /mnt/var  # Copy on write disabled
-mount ${part_boot} /mnt/boot  # Mount the boot partition
+if [[ $part == "no" ]]; then
+    pacman -Sy dialog --noconfirm                                                                  #install dialog for selecting disk
+    devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)                 #gets disk info for selection
+    drive=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1       #chose which drive to format
+    clear           # clears blue screen from
+    lsblk           # shows avalable drives
+    echo ${drive}   # confirms drive selection
+    part_boot="$(ls ${drive}* | grep -E "^${drive}p?1$")"     #finds boot partion
+    part_root="$(ls ${drive}* | grep -E "^${drive}p?2$")"     #finds root partion
+    cryptsetup luksOpen ${part_root} cryptroot  # Open the mapper
+    mount /dev/mapper/cryptroot /mnt
+    # clearing non home data
+    btrfs subvolume delete /mnt/@
+    btrfs subvolume delete /mnt/@pkg
+    btrfs subvolume delete /mnt/@var
+    btrfs subvolume delete /mnt/@srv
+    btrfs subvolume delete /mnt/@tmp
+    #creating new subvolumes
+    btrfs subvolume create /mnt/@
+    btrfs subvolume create /mnt/@pkg
+    btrfs subvolume create /mnt/@var
+    btrfs subvolume create /mnt/@srv
+    btrfs subvolume create /mnt/@tmp
+    umount /mnt
+    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@ /dev/mapper/cryptroot /mnt
+    mkdir -p /mnt2/{home,var/cache/pacman/pkg,var,srv,tmp,boot}  # Create directories for each subvolume
+    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@home /dev/mapper/cryptroot /mnt/home
+    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@pkg /dev/mapper/cryptroot /mnt/var/cache/pacman/pkg
+    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@var /dev/mapper/cryptroot /mnt/var
+    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@srv /dev/mapper/cryptroot /mnt/srv
+    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@tmp /dev/mapper/cryptroot /mnt/tmp
+    chattr +C /mnt/var  # Copy on write disabled
+    mount ${part_boot} /mnt/boot  # Mount the boot partition
+    
+    else
+    
+    pacman -Sy dialog --noconfirm                                                                  #install dialog for selecting disk
+    devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)                 #gets disk info for selection
+    drive=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1       #chose which drive to format
+    clear           # clears blue screen from 
+    lsblk           # shows avalable drives
+    echo ${drive}   # confirms drive selection
+    sgdisk --zap-all ${drive}  # Delete tables
+    printf "n\n1\n\n+333M\nef00\nn\n2\n\n\n\nw\ny\n" | gdisk ${drive}  # Format the drive
+    
+    part_boot="$(ls ${drive}* | grep -E "^${drive}p?1$")"     #finds boot partion
+    part_root="$(ls ${drive}* | grep -E "^${drive}p?2$")"     #finds root partion
+    
+    echo ${part_boot} # confirms boot partion selection
+    echo ${part_root} # confirms root partion selection
+    
+    mkdir -p -m0700 /run/cryptsetup  # Change permission to root only
+    cryptsetup luksFormat --type luks2 ${part_root}
+    cryptsetup luksOpen ${part_root} cryptroot  # Open the mapper
+    
+    mkfs.vfat -F32 ${part_boot}  # Format the EFI partition
+    mkfs.btrfs /dev/mapper/cryptroot  # Format the encrypted partition
+    
+    mount /dev/mapper/cryptroot /mnt
+    btrfs subvolume create /mnt/@
+    btrfs subvolume create /mnt/@home
+    btrfs subvolume create /mnt/@pkg
+    btrfs subvolume create /mnt/@var
+    btrfs subvolume create /mnt/@srv
+    btrfs subvolume create /mnt/@tmp
+    umount /mnt
+    
+    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@ /dev/mapper/cryptroot /mnt
+    mkdir -p /mnt/{home,var/cache/pacman/pkg,var,srv,tmp,boot}  # Create directories for each subvolume
+    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@home /dev/mapper/cryptroot /mnt/home
+    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@pkg /dev/mapper/cryptroot /mnt/var/cache/pacman/pkg
+    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@var /dev/mapper/cryptroot /mnt/var
+    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@srv /dev/mapper/cryptroot /mnt/srv
+    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@tmp /dev/mapper/cryptroot /mnt/tmp
+    chattr +C /mnt/var  # Copy on write disabled
+    mount ${part_boot} /mnt/boot  # Mount the boot partition
+    
+fi
 
 sed -i "/#Color/a ILoveCandy" /etc/pacman.conf  # Making pacman prettier
 sed -i "s/#Color/Color/g" /etc/pacman.conf  # Add color to pacman
