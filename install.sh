f1e74bacbc99aa5b2e51f45fd8defc4c7208bc1f
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2021-11-14 15:38:17 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -192,7 +192,7 @@ touch /etc/systemd/zram-generator.conf
 tee -a /etc/systemd/zram-generator.conf << END
 [zram0]
 zram-fraction = 1
-max-zram-size = 2048
+max-zram-size = 4096
 END
 
 touch /etc/sysctl.d/99-swappiness.conf
