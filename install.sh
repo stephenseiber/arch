098caad899410e5c633c0bf4e3ce9e1e3d24f1be
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2022-01-01 03:31:31 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -118,14 +118,15 @@ else
 
     mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@ ${part_root} /mnt
     mkdir -p /mnt/{home,var/cache/pacman/pkg,var,srv,tmp,boot}  # Create directories for each subvolume
+    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@home ${part_root} /mnt/home
     mkdir -p /mnt/home/$username
     mkdir -p /mnt/home/$username/Games
+    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@Games ${part_root} /mnt/home/$username/Games
     mkdir -p /mnt/home/$username/Videos
+    mkdir -p /mnt/home/$username/.local/
+    mkdir -p /mnt/home/$username/.local/share/
     mkdir -p /mnt/home/$username/.local/share/Steam/steamapps/
     mkdir -p /mnt/home/$username/Games/steamapps/
-    
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@home ${part_root} /mnt/home
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@Games ${part_root} /mnt/home/$username/Games
     mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@steamapps ${part_root} /mnt/home/$username/Games/steamapps/
     mount --bind /home/stephen/Games/steamapps/ /home/stephen/.local/share/Steam/steamapps/
     mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@pkg ${part_root} /mnt/var/cache/pacman/pkg
