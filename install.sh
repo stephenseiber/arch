6599d667897361fc14325140897a96c687fb2e42
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2022-01-18 22:02:21 -0700

    Update install.sh
    removed reinstall promt. havent figured out how to get it to work with snapshots.
    fixed baloo script problem

--- install.sh
+++ install.sh
@@ -1,145 +1,65 @@
 #!/usr/bin/env bash
 mount -o remount,size=8G /run/archiso/cowspace
-df -l
 clear   # Clear the TTY
 set -e  # The script will not run if we CTRL + C, or in case of an error
 set -u  # Treat unset variables as an error when substituting
 
-## This are the defaults, so it's easier to test the script
-# keymap=us
-# part=yes
-# username=csjarchlinux  # Can only be lowercase and no signs
-# hostname=desktop
-# password=csjarchlinux
-
-read -p "do you want to wipe full drive yes or no, or press enter to use defaults:"$'\n' part
-if [[ -z $part ]]; then
-    part=yes
-fi
-
 read -p "Enter user name, or press enter to use defaults:"$'\n' username
-if [[ -z $username ]]; then
-    username=csjarchlinux
-fi
-
-read -p "Enter host name, or press enter to use defaults:"$'\n' hostname
-if [[ -z $hostname ]]; then
-    hostname=csjarchlinux
-fi
-
 read -s -p "Enter user password, or press enter to use defaults:"$'\n' password
-if [[ -z $password ]]; then
-    password=csjarchlinux
-fi
+read -p "Enter host name, or press enter to use defaults:"$'\n' hostname
 
 timedatectl set-ntp true  # Synchronize motherboard clock
-
 keymap=us
 
-if [[ $part == "no" ]]; then
-    pacman -Sy dialog --noconfirm  # Install dialog for selecting disk
-    devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)  # Gets disk info for selection
-    drive=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1  # Chose which drive to format
-    clear  # Clears blue screen from
-    lsblk  # Shows avalable drives
-    echo ${drive}  # Confirms drive selection
-    part_boot="$(ls ${drive}* | grep -E "^${drive}p?1$")"  # Finds boot partion
-    part_root="$(ls ${drive}* | grep -E "^${drive}p?2$")"  # Finds root partion
-    mkfs.vfat -F32 ${part_boot}  # Format the EFI partition
-    mount ${part_root} /mnt
+pacman -Sy dialog archlinux-keyring --noconfirm  
+# Install dialog for selecting disk also updates keyring for if you are using older install disk
 
-    btrfs filesystem label ${part_root} arch
+devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)  # Gets disk info for selection
+drive=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1  # Chose which drive to format
+clear  # Clears blue screen from
 
-    # Clearing non home data
+sgdisk --zap-all ${drive}  # Delete tables
+printf "n\n1\n\n+512M\nef00\nn\n2\n\n\n\nw\ny\n" | gdisk ${drive}  # Format the drive
 
-    btrfs subvolume delete /mnt/@pkg
-    btrfs subvolume delete /mnt/@var/lib/portables
-    btrfs subvolume delete /mnt/@var/lib/machines
-    btrfs subvolume delete /mnt/@var
-    btrfs subvolume delete /mnt/@srv
-    btrfs subvolume delete /mnt/@tmp
-    btrfs subvolume delete /mnt/@/.snapshots
-    btrfs subvolume delete /mnt/@home/.snapshots
-    btrfs subvolume delete /mnt/@
+part_boot="$(ls ${drive}* | grep -E "^${drive}p?1$")"  # Finds boot partion
+part_root="$(ls ${drive}* | grep -E "^${drive}p?2$")"  # Finds root partion
 
-    # Creating new subvolumes
-    btrfs subvolume create /mnt/@
-    btrfs subvolume create /mnt/@pkg
-    btrfs subvolume create /mnt/@var
-    btrfs subvolume create /mnt/@srv
-    btrfs subvolume create /mnt/@tmp
+mkfs.vfat -F32 ${part_boot}  # Format the EFI partition
+mkfs.btrfs -f ${part_root}  # Format the encrypted partition
+btrfs filesystem label ${part_root} arch
 
+mount ${part_root} /mnt
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@pkg
+btrfs subvolume create /mnt/@var
+btrfs subvolume create /mnt/@srv
+btrfs subvolume create /mnt/@tmp
+btrfs subvolume create /mnt/@Games
+btrfs subvolume create /mnt/@steamapps
+btrfs subvolume create /mnt/@Videos
+umount /mnt
 
-    umount /mnt
-
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@ ${part_root} /mnt
-    mkdir -p /mnt/{home,var/cache/pacman/pkg,var,srv,tmp,boot}  # Create directories for each subvolume
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@home ${part_root} /mnt/home
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@Games ${part_root} /mnt/home/$username/Games
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@Videos ${part_root} /mnt/home/$username/Videos
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@steamapps ${part_root} /mnt/home/$username/Games/steamapps/
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@pkg ${part_root} /mnt/var/cache/pacman/pkg
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@var ${part_root} /mnt/var
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@srv ${part_root} /mnt/srv
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@tmp ${part_root} /mnt/tmp
-    chattr +C /mnt/var  # Copy on write disabled
-	mount ${part_boot} /mnt/boot  # Mount the boot partition
-else
-	pacman -Sy dialog --noconfirm  # Install dialog for selecting disk
-    devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)  # Gets disk info for selection
-    drive=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1  # Chose which drive to format
-    clear  # Clears blue screen from
-    lsblk  # Shows available drives
-    echo ${drive}  # Confirms drive selection
-    sgdisk --zap-all ${drive}  # Delete tables
-    printf "n\n1\n\n+512M\nef00\nn\n2\n\n\n\nw\ny\n" | gdisk ${drive}  # Format the drive
-
-    part_boot="$(ls ${drive}* | grep -E "^${drive}p?1$")"  # Finds boot partion
-    part_root="$(ls ${drive}* | grep -E "^${drive}p?2$")"  # Finds root partion
-
-    echo ${part_boot}  # Confirms boot partion selection
-    echo ${part_root}  # Confirms root partion selection
-
-   
-
-    mkfs.vfat -F32 ${part_boot}  # Format the EFI partition
-    mkfs.btrfs -f ${part_root}  # Format the encrypted partition
-
-    btrfs filesystem label ${part_root} arch
-
-    mount ${part_root} /mnt
-    btrfs subvolume create /mnt/@
-    btrfs subvolume create /mnt/@home
-    btrfs subvolume create /mnt/@pkg
-    btrfs subvolume create /mnt/@var
-    btrfs subvolume create /mnt/@srv
-    btrfs subvolume create /mnt/@tmp
-    btrfs subvolume create /mnt/@Games
-    btrfs subvolume create /mnt/@steamapps
-    btrfs subvolume create /mnt/@Videos
-    umount /mnt
-
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@ ${part_root} /mnt
-    mkdir -p /mnt/{home,var/cache/pacman/pkg,var,srv,tmp,boot}  # Create directories for each subvolume
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@home ${part_root} /mnt/home
-    mkdir -p /mnt/home/$username
-    mkdir -p /mnt/home/$username/Games
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@Games ${part_root} /mnt/home/$username/Games
-    mkdir -p /mnt/home/$username/Videos
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@Videos ${part_root} /mnt/home/$username/Videos
-    mkdir -p /mnt/home/$username/.local/
-    mkdir -p /mnt/home/$username/.local/share/
-    mkdir -p /mnt/home/$username/.local/share/Steam/steamapps/
-    mkdir -p /mnt/home/$username/Games/steamapps/
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@steamapps ${part_root} /mnt/home/$username/Games/steamapps/
-    mount --bind /mnt/home/stephen/Games/steamapps/ /mnt/home/stephen/.local/share/Steam/steamapps/
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@pkg ${part_root} /mnt/var/cache/pacman/pkg
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@var ${part_root} /mnt/var
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@srv ${part_root} /mnt/srv
-    mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@tmp ${part_root} /mnt/tmp
-    chattr +C /mnt/var  # Copy on write disabled
-    mount ${part_boot} /mnt/boot  # Mount the boot partition
-fi
+mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@ ${part_root} /mnt
+mkdir -p /mnt/{home,var/cache/pacman/pkg,var,srv,tmp,boot}  # Create directories for each subvolume
+mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@home ${part_root} /mnt/home
+mkdir -p /mnt/home/$username
+mkdir -p /mnt/home/$username/Games
+mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@Games ${part_root} /mnt/home/$username/Games
+mkdir -p /mnt/home/$username/Videos
+mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@Videos ${part_root} /mnt/home/$username/Videos
+mkdir -p /mnt/home/$username/.local/
+mkdir -p /mnt/home/$username/.local/share/
+mkdir -p /mnt/home/$username/.local/share/Steam/steamapps/
+mkdir -p /mnt/home/$username/Games/steamapps/
+mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@steamapps ${part_root} /mnt/home/$username/Games/steamapps/
+mount --bind /mnt/home/$username/Games/steamapps/ /mnt/home/$username/.local/share/Steam/steamapps/
+mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@pkg ${part_root} /mnt/var/cache/pacman/pkg
+mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@var ${part_root} /mnt/var
+mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@srv ${part_root} /mnt/srv
+mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@tmp ${part_root} /mnt/tmp
+chattr +C /mnt/var  # Copy on write disabled
+mount ${part_boot} /mnt/boot  # Mount the boot partition
 
 sed -i "/#Color/a ILoveCandy" /etc/pacman.conf  # Making pacman prettier
 sed -i "s/#Color/Color/g" /etc/pacman.conf  # Add color to pacman
@@ -148,7 +68,6 @@ sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf # multilib
 
 reflector --latest 50 --verbose --protocol https --sort rate --save /etc/pacman.d/mirrorlist -c US --ipv6
 pacman -Syy
-
 
 pacstrap -i /mnt --noconfirm base base-devel linux linux-firmware linux-headers git nano fish \
     intel-ucode networkmanager efibootmgr btrfs-progs zram-generator \
@@ -176,7 +95,6 @@ locale-gen
 
 echo -e "127.0.0.1\tlocalhost" > /etc/hosts
 echo -e "::1\t\tlocalhost" >> /etc/hosts
-
 echo -e "KEYMAP=$keymap" > /etc/vconsole.conf
 sed -i "s/# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/g" /etc/sudoers
 sed -i "/#Color/a ILoveCandy" /etc/pacman.conf
@@ -188,8 +106,8 @@ echo -e "$hostname" > /etc/hostname
 useradd -m -g users -G wheel -s /bin/fish $username
 echo -en "$password\n$password" | passwd
 echo -en "$password\n$password" | passwd $username
-
 useradd -g users -G wheel -m temp
+
 sudo -u temp mkdir -p /tmp/yay && cd /tmp/yay && sudo -u temp git clone https://aur.archlinux.org/yay.git && cd yay && sudo -u temp makepkg -si --noconfirm
 sudo -u temp yay -S epson-inkjet-printer-escpr --noconfirm
 sudo -u temp yay -S ttf-ms-fonts --noconfirm
@@ -203,8 +121,11 @@ tee -a /home/$username/.config/baloofilerc << END
 dbVersion=2
 exclude filters=*~,*.part,*.o,*.la,*.lo,*.loT,*.moc,moc_*.cpp,qrc_*.cpp,ui_*.h,cmake_install.cmake,CMakeCache.txt,CTestTestfile.cmake,libtool,config.status,confdefs.h,autom4te,conftest,confstat,Makefile.am,*.gcode,.ninja_deps,.ninja_log,build.ninja,*.csproj,*.m4,*.rej,*.gmo,*.pc,*.omf,*.aux,*.tmp,*.po,*.vm*,*.nvram,*.rcore,*.swp,*.swap,lzo,litmain.sh,*.orig,.histfile.*,.xsession-errors*,*.map,*.so,*.a,*.db,*.qrc,*.ini,*.init,*.img,*.vdi,*.vbox*,vbox.log,*.qcow2,*.vmdk,*.vhd,*.vhdx,*.sql,*.sql.gz,*.ytdl,*.class,*.pyc,*.pyo,*.elc,*.qmlc,*.jsc,*.fastq,*.fq,*.gb,*.fasta,*.fna,*.gbff,*.faa,po,CVS,.svn,.git,_darcs,.bzr,.hg,CMakeFiles,CMakeTmp,CMakeTmpQmake,.moc,.obj,.pch,.uic,.npm,.yarn,.yarn-cache,__pycache__,node_modules,node_packages,nbproject,core-dumps,lost+found
 exclude filters version=8
-exclude folders[$e]=$HOME/Games/
+exclude folders[$~]=$~home/Games/
 END
+
+sed -i 's/~home/HOME'/g /home/$username/.config/baloofilerc
+sed -i 's/~]/e]'/g /home/$username/.config/baloofilerc
 
 mkdir -p /home/$username/.config/fish
 touch /home/$username/.config/fish/config.fish
@@ -215,16 +136,14 @@ if status is-interactive
 end
 END
 
-
 cd /tmp && touch panel-restart && echo '#!/bin/bash' > panel-restart && echo 'killall plasmashell;plasmashell &' >> panel-restart && chmod +x panel-restart && mv panel-restart /usr/bin/
 touch reflector-update && echo '#!/bin/bash' > reflector-update && echo 'sudo reflector --latest 50 --verbose --protocol https --sort rate --save /etc/pacman.d/mirrorlist -c US --ipv6' >> reflector-update && chmod +x reflector-update && mv reflector-update /usr/bin
 userdel -r temp
 
-systemctl enable NetworkManager fstrim.timer sddm bluetooth cups snapper-timeline.timer snapper-cleanup.timer 
-
+systemctl enable NetworkManager fstrim.timer sddm bluetooth cups snapper-timeline.timer snapper-cleanup.timer
+ 
 snapper -c root --no-dbus create-config /
 snapper -c home --no-dbus create-config /home
-
 sed -i 's/ALLOW_GROUPS=""/ALLOW_GROUPS="wheel"'/g /etc/snapper/configs/root
 sed -i 's/TIMELINE_LIMIT_HOURLY="10"/TIMELINE_LIMIT_HOURLY="16"'/g /etc/snapper/configs/root
 sed -i 's/TIMELINE_LIMIT_DAILY="10"/TIMELINE_LIMIT_DAILY="7"'/g /etc/snapper/configs/root
@@ -237,13 +156,12 @@ sed -i 's/TIMELINE_LIMIT_DAILY="10"/TIMELINE_LIMIT_DAILY="7"'/g /etc/snapper/con
 sed -i 's/TIMELINE_LIMIT_WEEKLY="0"/TIMELINE_LIMIT_WEEKLY="4"'/g /etc/snapper/configs/home
 sed -i 's/TIMELINE_LIMIT_MONTHLY="10"/TIMELINE_LIMIT_MONTHLY="1"'/g /etc/snapper/configs/home
 sed -i 's/TIMELINE_LIMIT_YEARLY="10"/TIMELINE_LIMIT_YEARLY="0"'/g /etc/snapper/configs/home
-
 chown -R :wheel /home/.snapshots/
 
 journalctl --vacuum-size=100M --vacuum-time=2weeks
-
 touch /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
 tee -a /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf << END
+
 [connection]
 wifi.powersave = 2
 END
@@ -265,7 +183,6 @@ tee -a /etc/pacman.d/hooks/100-systemd-boot.hook << END
 Type = Package
 Operation = Upgrade
 Target = systemd
-
 [Action]
 Description = Updating systemd-boot
 When = PostTransaction
@@ -274,7 +191,6 @@ END
 
 touch /etc/pacman.d/hooks/nvidia.hook
 tee -a /etc/pacman.d/hooks/nvidia.hook << END
-  
 [Trigger]
 Operation=Install
 Operation=Upgrade
@@ -282,17 +198,16 @@ Operation=Remove
 Type=Package
 Target=nvidia
 Target=linux
-
 [Action]
 Depends=mkinitcpio
 When=PostTransaction
 Exec=/usr/bin/mkinitcpio -p linux
 END
 
-base udev autodetect modconf block filesystems keyboard fsck
 sed -i "s/^HOOKS.*/HOOKS=(base udev autodetect modconf block btrfs filesystems keyboard fsck)/g" /etc/mkinitcpio.conf
 sed -i 's/^MODULES.*/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
 mkinitcpio -P
+
 bootctl --path=/boot/ install
 
 mkdir -p /boot/loader/
@@ -310,9 +225,7 @@ linux /vmlinuz-linux
 initrd /intel-ucode.img
 initrd /initramfs-linux.img
 options root="LABEL=arch" rootflags=subvol=@ rw nvidia-drm.modeset=1
-
 END
-
 
 chsh -s /bin/fish
 pacman-key --init
@@ -321,11 +234,5 @@ pip install requests vdf
 sudo chown -R $username /home/$username/
 EOF
 
-read -p "Do you wish to reboot? type y for yes"$'\n' -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-echo "rebooting now"
-reboot
-else echo "script has finished"
-fi
- 
+echo "script has finished"
+
