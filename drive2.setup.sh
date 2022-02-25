efba947d83cfe8bcfe0a7248726b4988238cddd9
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2022-02-25 16:37:02 -0700

    Update drive2.setup.sh

--- drive2.setup.sh
+++ drive2.setup.sh
@@ -1,8 +1,7 @@
 devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)  # Gets disk info for selection
 drive2=$(dialog --stdout --menu "Select second drive" 0 0 0 ${devicelist}) || exit 1  # Chose which 
 clear
-#drive2p="$(ls ${drive}* | grep -E "^${drive}p?3$")"  # Finds partition
-drive2p=${drive2}1
+drive2p="$(ls ${drive}* | grep -E "^${drive}p?1$")"  # Finds partition
 mount ${drive2p} /mnt
 btrfs subvolume create /mnt/@"Y'shtola"
 btrfs subvolume create /mnt/@Alphinaud
