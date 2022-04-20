e024d7d9bb68784f52e8ef88880daa1eb1db3852
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2022-04-20 10:27:51 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,10 +18,15 @@ devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)  
 drive=$(dialog --stdout --menu "Select installation disk" 0 0 0 ${devicelist}) || exit 1  # Chose which drive to format
 clear  # Clears blue screen
 
-#devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)  # Gets disk info for selection
-#drive2=$(dialog --stdout --menu "Select second drive" 0 0 0 ${devicelist}) || exit 1  # Chose which 
-#clear # Clears blue screen
-#drive2p="$(ls ${drive2}* | grep -E "^${drive2}p?1$")"  # Finds partition
+devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)  # Gets disk info for selection
+drive2=$(dialog --stdout --menu "Select second drive" 0 0 0 ${devicelist}) || exit 1  # Chose which 
+clear # Clears blue screen
+drive2p="$(ls ${drive2}* | grep -E "^${drive2}p?1$")"  # Finds partition
+
+devicelist=$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tac)  # Gets disk info for selection
+drive3=$(dialog --stdout --menu "Select second drive" 0 0 0 ${devicelist}) || exit 1  # Chose which 
+clear # Clears blue screen
+drive3p="$(ls ${drive2}* | grep -E "^${drive2}p?1$")"  # Finds partition
 
 sgdisk --zap-all ${drive}  # Delete tables
 printf "n\n1\n\n+512M\nef00\nn\n2\n\n\n\nw\ny\n" | gdisk ${drive}  # Format the drive
@@ -50,24 +55,28 @@ mkdir -p /mnt/{home,var/cache/pacman/pkg,var,srv,tmp,boot}  # Create directories
 mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@home ${part_root} /mnt/home
 mkdir -p /mnt/home/$username
 
-#mkdir -p /mnt/home/$username/"G'raha"
-#mkdir -p /mnt/home/$username/"G'raha"/Alphinaud
-#mkdir -p /mnt/home/$username/"G'raha"/Thancred
-#mkdir -p /mnt/home/$username/"G'raha"/"Y'shtola"
-#mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@Alphinaud ${drive2p} /mnt/home/$username/"G'raha"/Alphinaud
-#mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@Thancred ${drive2p} /mnt/home/$username/"G'raha"/Thancred
-#mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@"Y'shtola" ${drive2p} /mnt/home/$username/"G'raha"/"Y'shtola"
+mkdir -p /mnt/home/$username/"G'raha"
+mkdir -p /mnt/home/$username/"G'raha"/Rudeus
+mkdir -p /mnt/home/$username/"G'raha"/Alphinaud
+mkdir -p /mnt/home/$username/"G'raha"/Thancred
+mkdir -p /mnt/home/$username/"G'raha"/"Y'shtola"
+mkdir -p /mnt/home/$username/"G'raha"/Rimuru
 
-mkdir -p /mnt/home/$username/Games
-mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@Games ${part_root} /mnt/home/$username/Games
+mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@Alphinaud ${drive2p} /mnt/home/$username/"G'raha"/Alphinaud
+mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@Thancred ${drive2p} /mnt/home/$username/"G'raha"/Thancred
+mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@"Y'shtola" ${drive2p} /mnt/home/$username/"G'raha"/"Y'shtola"
+mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@Rimuru ${drive3p} /mnt/home/$username/"G'raha"/Rimuru
+
+mkdir -p /mnt/home/$username/"G'raha"/Rudeus/
+mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@Games ${part_root} mkdir -p /mnt/home/$username/"G'raha"/Rudeus/
 mkdir -p /mnt/home/$username/Videos
 mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@Videos ${part_root} /mnt/home/$username/Videos
 mkdir -p /mnt/home/$username/.local/
 mkdir -p /mnt/home/$username/.local/share/
 mkdir -p /mnt/home/$username/.local/share/Steam/steamapps/
-mkdir -p /mnt/home/$username/Games/steamapps/
-mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@steamapps ${part_root} /mnt/home/$username/Games/steamapps/
-mount --bind /mnt/home/$username/Games/steamapps/ /mnt/home/$username/.local/share/Steam/steamapps/
+mkdir -p /mnt/home/$username/"G'raha"/Rudeus/steamapps/
+mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@steamapps ${part_root} /mnt/home/$username/"G'raha"/Rudeus/steamapps/
+mount --bind /mnt/home/$username/"G'raha"/Rudeus/steamapps/ /mnt/home/$username/.local/share/Steam/steamapps/
 mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@pkg ${part_root} /mnt/var/cache/pacman/pkg
 mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@var ${part_root} /mnt/var
 mount -o noatime,compress-force=zstd:1,space_cache=v2,subvol=@srv ${part_root} /mnt/srv
