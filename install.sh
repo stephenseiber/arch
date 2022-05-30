4b7f72834ce2a5493cb9de6ae1ab0518c7182ca6
stephenseiber <2262954+stephenseiber@users.noreply.github.com>
2022-05-30 00:02:31 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -252,8 +252,7 @@ chsh -s /bin/fish
 pacman-key --init
 pacman-key --populate archlinux
 pip install requests vdf
-#read -p "Did the install have no issues"$'\n' test
-#sudo chown -R $username /home/$username/
+sudo chown -R $username /home/$username/
 EOF
 
 echo "script has finished"
